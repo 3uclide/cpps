a3ec4a94041df21a922532f8b80e977597f6a9b9
3uclide <3uclide@gmail.com>
2022-11-05 14:40:43 -0400

    Source
    Contains a list of typed lines to be process by the lexer

---
+++ src/cpps/source.hpp
@@ -0,0 +1,142 @@
+#pragma once
+
+#include "cpps/source-location.hpp"
+
+#include <range/v3/view.hpp>
+#include <string>
+#include <vector>
+
+namespace cpps {
+
+class Source
+{
+public:
+    class Line
+    {
+    public:
+        enum class Type
+        {
+            Cpp,
+            Cpps,
+            Comment,
+            Empty,
+            Import,
+            Preprocessor
+        };
+
+    public:
+        Line(std::string text, Type type);
+
+        Type getType() const;
+
+        const std::string& getText() const;
+
+        bool empty() const;
+        SourceColumn size() const;
+
+    private:
+        friend class Source;
+
+    private:
+        std::string _text;
+        Type _type;
+    };
+
+public:
+    bool hasCpp() const;
+    bool hasCpps() const;
+
+    bool empty() const;
+    SourceLine size() const;
+
+    const Line& operator[](SourceLine line) const;
+
+    void add(std::string text, Line::Type type);
+
+    // set previous adjacent comment/empty line type to cpps
+    void cppsify();
+
+private:
+    std::vector<Line> _lines;
+
+    bool _hasCpp{false};
+    bool _hasCpps{false};
+};
+
+inline Source::Line::Line(std::string text, Type type)
+    : _text(std::move(text))
+    , _type(type)
+{
+}
+
+inline Source::Line::Type Source::Line::getType() const
+{
+    return _type;
+}
+
+inline const std::string& Source::Line::getText() const
+{
+    return _text;
+}
+
+inline bool Source::Line::empty() const
+{
+    return _text.empty();
+}
+
+inline SourceColumn Source::Line::size() const
+{
+    return static_cast<SourceColumn>(_text.size());
+}
+
+inline bool Source::hasCpp() const
+{
+    return _hasCpp;
+}
+
+inline bool Source::hasCpps() const
+{
+    return _hasCpps;
+}
+
+inline bool Source::empty() const
+{
+    return _lines.empty();
+}
+
+inline SourceLine Source::size() const
+{
+    return static_cast<SourceLine>(_lines.size());
+}
+
+inline const Source::Line& Source::operator[](SourceLine line) const
+{
+    return _lines[line];
+}
+
+inline void Source::add(std::string text, Line::Type type)
+{
+    _lines.emplace_back(std::move(text), type);
+
+    _hasCpp |= type == Line::Type::Cpp || type == Line::Type::Preprocessor;
+    _hasCpps |= type == Line::Type::Cpps;
+}
+
+inline void Source::cppsify()
+{
+    assert(_lines.back().getType() == Line::Type::Cpps);
+
+    for (Line& line : ranges::views::reverse(_lines) | ranges::views::drop(1))
+    {
+        const Line::Type lineType = line.getType();
+
+        if (lineType != Line::Type::Comment && lineType != Line::Type::Empty)
+        {
+            break;
+        }
+
+        line._type = Line::Type::Cpps;
+    }
+}
+
+} // namespace cpps
---
+++ test/unit-tests/cpps/source-tests.cpp
@@ -0,0 +1,107 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "cpps/source.hpp"
+
+namespace cpps {
+
+TEST_CASE("Source default ctor", "[Source]")
+{
+    const Source source;
+
+    CHECK_FALSE(source.hasCpp());
+    CHECK_FALSE(source.hasCpps());
+    CHECK(source.empty());
+}
+
+TEST_CASE("Source add", "[Source]")
+{
+    Source source;
+
+    SECTION("will add line text and type")
+    {
+        static constexpr std::string_view CommentLine = "comment";
+        static constexpr std::string_view EmptyLine = "";
+        static constexpr std::string_view ImportLine = "import";
+
+        source.add(std::string(CommentLine), Source::Line::Type::Comment);
+        source.add(std::string(EmptyLine), Source::Line::Type::Empty);
+        source.add(std::string(ImportLine), Source::Line::Type::Import);
+
+        REQUIRE(source.size() == 3);
+
+        CHECK(source[0].getType() == Source::Line::Type::Comment);
+        CHECK(source[0].getText() == CommentLine);
+        CHECK(source[0].empty() == CommentLine.empty());
+        CHECK(source[0].size() == CommentLine.size());
+
+        CHECK(source[1].getType() == Source::Line::Type::Empty);
+        CHECK(source[1].getText() == EmptyLine);
+        CHECK(source[1].empty() == EmptyLine.empty());
+        CHECK(source[1].size() == EmptyLine.size());
+
+        CHECK(source[2].getType() == Source::Line::Type::Import);
+        CHECK(source[2].getText() == ImportLine);
+        CHECK(source[2].empty() == ImportLine.empty());
+        CHECK(source[2].size() == ImportLine.size());
+    }
+
+    SECTION("of type cpp will set source has cpp")
+    {
+        CHECK_FALSE(source.hasCpp());
+
+        source.add("line", Source::Line::Type::Cpp);
+
+        CHECK(source.hasCpp());
+    }
+
+    SECTION("of type preprocessor will set source has cpp")
+    {
+        CHECK_FALSE(source.hasCpp());
+
+        source.add("line", Source::Line::Type::Preprocessor);
+
+        CHECK(source.hasCpp());
+    }
+
+    SECTION("of type cpps will set source has cpps")
+    {
+        CHECK_FALSE(source.hasCpps());
+
+        source.add("line", Source::Line::Type::Cpps);
+
+        CHECK(source.hasCpps());
+    }
+}
+
+TEST_CASE("Source cppsify", "[Source]")
+{
+    Source source;
+
+    SECTION("do nothing if singe line")
+    {
+        source.add("cpps", Source::Line::Type::Cpps);
+
+        source.cppsify();
+
+        REQUIRE(source.size() == 1);
+        REQUIRE(source[0].getType() == Source::Line::Type::Cpps);
+    }
+
+    SECTION("set previous adjacent comment/empty line type to cpps")
+    {
+        source.add("cpp", Source::Line::Type::Cpp);
+        source.add("comment", Source::Line::Type::Comment);
+        source.add("", Source::Line::Type::Empty);
+        source.add("cpps", Source::Line::Type::Cpps);
+
+        source.cppsify();
+
+        REQUIRE(source.size() == 4);
+        REQUIRE(source[0].getType() == Source::Line::Type::Cpp);
+        REQUIRE(source[1].getType() == Source::Line::Type::Cpps);
+        REQUIRE(source[2].getType() == Source::Line::Type::Cpps);
+        REQUIRE(source[3].getType() == Source::Line::Type::Cpps);
+    }
+}
+
+} // namespace cpps
