6021cbeaf13ea40b532f0734a3721d96b5d0366f
3uclide <3uclide@gmail.com>
2022-11-06 00:43:53 -0400

    Added SourceLocation std::ostream unit tests

--- test/unit-tests/cpps/source-location-tests.cpp
+++ test/unit-tests/cpps/source-location-tests.cpp
@@ -1,6 +1,7 @@
-#include <catch2/catch_test_macros.hpp>
-
 #include "cpps/source-location.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <sstream>
 
 namespace cpps {
 
@@ -16,6 +17,22 @@ TEST_CASE("SourceLocation format", "[SourceLocation]")
     CHECK(fmt::format("{}", SourceLocation{.line = 42, .column = 24}) == "42, 24");
     CHECK(fmt::format("{}", SourceLocation{.line = 42, .column = InvalidSourceColumn}) == "42");
     CHECK(fmt::format("{}", InvalidSourceLocation) == "invalid");
+}
+
+TEST_CASE("SourceLocation ostream", "[SourceLocation]")
+{
+    std::stringstream stream;
+
+    stream << SourceLocation{0, 0} << "\n";
+    stream << std::optional<SourceLocation>{{0, 0}} << "\n";
+    stream << std::optional<SourceLocation>{} << "\n";
+
+    std::string expectedString =
+        "0, 0\n"
+        "0, 0\n"
+        "nullopt\n";
+
+    CHECK(stream.str() == expectedString);
 }
 
 } // namespace cpps
